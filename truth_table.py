48514aa8a82e2889fdc34a606be49981f71a84c1
Josh <joshuahbronner@gmail.com>
2025-04-23 23:41:52 -0400

    Refactored parser, specified types better

--- truth_table.py
+++ truth_table.py
@@ -1,24 +1,24 @@
 from itertools import product
-from typing import List
+from typing import List, Optional
 from expressions import *
 from tokens import *
 
 class TruthTable:
-    def __init__(self, input_statement:str):
+    def __init__(self, input_statement:str) -> None:
         self._statement:List[Token|Subexpression] = self._tokenize(input_statement)
         self._table = self._create_table(self._statement)
 
     # FIXME Either values must be set to functions reference or dict must be changed to list (or removed entirely)
     _UNARY_OPERATORS = {
-        '¬': Not
+        '¬': TokenType.NOT
     }
 
     # FIXME Either values must be set to functions reference or dict must be changed to list (or removed entirely)
     _BINARY_OPERATORS = {
-        'v': Disjunction,
-        '^': Conjunction,
-        '⇒': Implies,
-        '⇔': Biconditional
+        'v': TokenType.DISJUNCTION,
+        '^': TokenType.CONJUNCTION,
+        '⇒': TokenType.IMPLIES,
+        '⇔': TokenType.BICONDITIONAL
     }
     
     # Takes in String statement, returns tokenized statement in list of dictionaries. Sublists are parenthesis
@@ -28,9 +28,9 @@ class TruthTable:
 
         for i, char in enumerate(statement):
             if char in self._UNARY_OPERATORS:
-                tokens.append(Token(TokenType.UNARY, char, i))
+                tokens.append(Token(self._UNARY_OPERATORS[char], char, i))
             elif char in self._BINARY_OPERATORS:
-                tokens.append(Token(TokenType.BINARY, char, i))
+                tokens.append(Token(self._BINARY_OPERATORS[char], char, i))
             elif char.isalpha():
                 tokens.append(Token(TokenType.ATOM, char, i))
             elif char == '(':
@@ -43,33 +43,16 @@ class TruthTable:
                 tokens.append(subexpression)
 
         return tokens
-
-    # Takes in list of tokens (parsed statement), returns statement table
-    # FIXME refactor
-    def _create_table(self, tokens:List[Token|Subexpression]) -> List[dict[str:bool]]:
-        truths:List[dict] = []
-
-        
-        truths.extend(self._extract_atoms(tokens))
     
     # Takes in list of tokens, returns all possible combinations of atomic truths in list of dicts
-    def _extract_atoms(self, tokens:List[Token], atoms:List[str]=[]) -> List[dict]:
+    def _extract_atoms(self, tokens:List[Token], collected_atoms:Optional[List[str]]=None) -> List[dict]:
+        if collected_atoms == None:
+            collected_atoms = []
         for token in tokens:
-            if token.type == TokenType.ATOM and token.value not in atoms:
-                atoms.append(token.value)
-            elif token.type == 'SUBEXPRESSION':
-                self._extract_atoms(token.value, atoms)
-        combinations = list(product([True, False], repeat=len(atoms)))
-        atomic_truths = [dict(zip(atoms, combination)) for combination in combinations]
+            if token.type == TokenType.ATOM and token.value not in collected_atoms:
+                collected_atoms.append(token.value)
+            elif token.type == TokenType.SUBEXPRESSION:
+                self._extract_atoms(token.value, collected_atoms)
+        combinations = list(product([True, False], repeat=len(collected_atoms)))
+        atomic_truths = [dict(zip(collected_atoms, combination)) for combination in combinations]
         return atomic_truths
-
-    # Takes in a list of tokens, returns a list of expressions (dicts)
-    # FIXME add functionality
-    def _parse_expression(self, tokens:List[Token|Subexpression]) -> Expression:
-        pass
-
-    # Returns truth table
-    # FIXME add functionality
-    def get_table(self) -> List[dict[str:bool]]:
-        pass 
-
