754563f908cbf9c30250395ab386d9c397b96859
Josh <joshuahbronner@gmail.com>
2025-04-24 19:16:56 -0400

    Added string representations for truth table support

--- expressions.py
+++ expressions.py
@@ -14,39 +14,49 @@ class BinaryOperator(Expression):
 class Atom(Expression):
     def __init__(self, name:str):
         self.name = name
+        self.left = self.name
+        self.right = self.name # These lines are stupid
+    def __str__(self):
+        return self.name
     def evaluate(self, assignment) -> bool:
         return assignment[self.name]
     
 class Not(Expression):
     def __init__(self, right:Expression):
         self.right = right
+    def __str__(self):
+        return f'¬{self.right}'
     def evaluate(self, assignment) -> bool:
         return not self.right.evaluate(assignment)
     
 class Conjunction(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
+    def __str__(self):
+        return f"{self.left}∧{self.right}"
     def evaluate(self, assignment):
         return self.left.evaluate(assignment) and self.right.evaluate(assignment)
     
 class Disjunction(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
+    def __str__(self):
+        return f"{self.left}∨{self.right}"
     def evaluate(self, assignment):
         return self.left.evaluate(assignment) or self.right.evaluate(assignment)
 
 class Implies(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
+    def __str__(self):
+        return f"{self.left}⇒{self.right}"
     def evaluate(self, assignment):
-        if self.left.evaluate(assignment) == False:
-            return True
-        elif self.right.evaluate(assignment) == True:
-            return True
-        return False
+        return not self.left.evaluate(assignment) or self.right.evaluate(assignment)
 
 class Biconditional(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
+    def __str__(self):
+        return f"{self.left}⇔{self.right}"
     def evaluate(self, assignment):
         return self.left.evaluate(assignment) == self.right.evaluate(assignment)
