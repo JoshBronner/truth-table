960b1955777a35ee3da3068b6779ce9ee400b971
Josh <joshuahbronner@gmail.com>
2025-04-24 09:50:59 -0400

    Refactored, rewriting. NOT COMPLETE.

--- truth_table.py
+++ truth_table.py
@@ -1,58 +1,31 @@
 from itertools import product
-from typing import List, Optional
+from typing import List
 from expressions import *
 from tokens import *
+from expression_parse import ExpressionParser
+from lexer import lexer
+from atom_extraction import extract_atoms
 
 class TruthTable:
     def __init__(self, input_statement:str) -> None:
-        self._statement:List[Token|Subexpression] = self._tokenize(input_statement)
-        self._table = self._create_table(self._statement)
+        self._tokens = lexer(input_statement)
+        self._expression = ExpressionParser(self._tokens).parsed_expression
+        self._atoms = extract_atoms(self._expression)
+        self._combinations = list(product([True, False], repeat=len(self._atoms)))
+        self._assignments = [dict(zip(self._atoms, combination)) for combination in self._combinations]
+        
+        self.truth_table = self.generate_truth_table(self._expression)
 
-    # FIXME Either values must be set to functions reference or dict must be changed to list (or removed entirely)
-    _UNARY_OPERATORS = {
-        '¬': TokenType.NOT
-    }
 
-    # FIXME Either values must be set to functions reference or dict must be changed to list (or removed entirely)
-    _BINARY_OPERATORS = {
-        'v': TokenType.DISJUNCTION,
-        '^': TokenType.CONJUNCTION,
-        '⇒': TokenType.IMPLIES,
-        '⇔': TokenType.BICONDITIONAL
-    }
+    def generate_truth_table(self) -> List[dict[str:bool]]:
+        table = []
+
+        for assignment in self._assignments:
+            eval_case = self._expression.evaluate(assignment)
+            row = {**assignment, str(self._expression):eval_case}
+            table.append(row)
+
+        return table
     
-    # Takes in String statement, returns tokenized statement in list of dictionaries. Sublists are parenthesis
-    def _tokenize(self, statement:str) -> List[Token|Subexpression]:
-        tokens:List[Token] = []
-        stack:List[List[Token]] = []
 
-        for i, char in enumerate(statement):
-            if char in self._UNARY_OPERATORS:
-                tokens.append(Token(self._UNARY_OPERATORS[char], char, i))
-            elif char in self._BINARY_OPERATORS:
-                tokens.append(Token(self._BINARY_OPERATORS[char], char, i))
-            elif char.isalpha():
-                tokens.append(Token(TokenType.ATOM, char, i))
-            elif char == '(':
-                stack.append((tokens, i))
-                tokens = []
-            elif char == ')':
-                previous_tokens, position = stack.pop()
-                subexpression = Subexpression(tokens, position)
-                tokens = previous_tokens
-                tokens.append(subexpression)
-
-        return tokens
-    
-    # Takes in list of tokens, returns all possible combinations of atomic truths in list of dicts
-    def _extract_atoms(self, tokens:List[Token], collected_atoms:Optional[List[str]]=None) -> List[dict]:
-        if collected_atoms == None:
-            collected_atoms = []
-        for token in tokens:
-            if token.type == TokenType.ATOM and token.value not in collected_atoms:
-                collected_atoms.append(token.value)
-            elif token.type == TokenType.SUBEXPRESSION:
-                self._extract_atoms(token.value, collected_atoms)
-        combinations = list(product([True, False], repeat=len(collected_atoms)))
-        atomic_truths = [dict(zip(collected_atoms, combination)) for combination in combinations]
-        return atomic_truths
+print(TruthTable("AvB").truth_table)
