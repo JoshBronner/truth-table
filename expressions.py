aa7646f30f6b59b6a38290a2a48afbd0cbef5c4d
Josh <joshuahbronner@gmail.com>
2025-04-25 08:24:42 -0400

    Refactored Expression subclasses.

--- expressions.py
+++ expressions.py
@@ -8,6 +8,16 @@ class BinaryOperator(Expression):
     def __init__(self, left:Expression, right:Expression):
         self.left:Expression = left
         self.right:Expression = right
+        self.symbol = ''
+    def __str__(self):
+        if isinstance(self.left, (Atom, Not)) and isinstance(self.right, (Atom, Not)):
+            return f"{self.left}{self.symbol}{self.right}"
+        elif isinstance(self.left, (Atom, Not)):
+            return f"{self.left}{self.symbol}({self.right})"
+        elif isinstance(self.right, (Atom, Not)):
+            return f"({self.left}){self.symbol}{self.right}"
+        else:
+            return f"({self.left}){self.symbol}({self.right})"
     def evaluate(self, assignment):
         raise NotImplementedError("Abstract class used")
 
@@ -15,7 +25,7 @@ class Atom(Expression):
     def __init__(self, name:str):
         self.name = name
         self.left = self.name
-        self.right = self.name # These lines are stupid
+        self.right = self.name
     def __str__(self):
         return self.name
     def evaluate(self, assignment) -> bool:
@@ -25,6 +35,8 @@ class Not(Expression):
     def __init__(self, right:Expression):
         self.right = right
     def __str__(self):
+        if isinstance(self.right, Atom):
+            return f'¬{self.right}'
         return f'¬{self.right}'
     def evaluate(self, assignment) -> bool:
         return not self.right.evaluate(assignment)
@@ -32,31 +44,27 @@ class Not(Expression):
 class Conjunction(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
-    def __str__(self):
-        return f"{self.left}∧{self.right}"
+        self.symbol = '∧'
     def evaluate(self, assignment):
         return self.left.evaluate(assignment) and self.right.evaluate(assignment)
     
 class Disjunction(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
-    def __str__(self):
-        return f"{self.left}∨{self.right}"
+        self.symbol = '∨'
     def evaluate(self, assignment):
         return self.left.evaluate(assignment) or self.right.evaluate(assignment)
 
 class Implies(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
-    def __str__(self):
-        return f"{self.left}⇒{self.right}"
+        self.symbol = '⇒'
     def evaluate(self, assignment):
         return not self.left.evaluate(assignment) or self.right.evaluate(assignment)
 
 class Biconditional(BinaryOperator):
     def __init__(self, left, right):
         super().__init__(left, right)
-    def __str__(self):
-        return f"{self.left}⇔{self.right}"
+        self.symbol = '⇔'
     def evaluate(self, assignment):
         return self.left.evaluate(assignment) == self.right.evaluate(assignment)
