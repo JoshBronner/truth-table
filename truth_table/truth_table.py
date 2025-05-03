6051ab39e308c68a5ce1dc7508926f1217af6ef2
Josh <joshuahbronner@gmail.com>
2025-05-03 19:21:29 -0400

    Created (buggy) remake of TruthTable, not in use

--- truth_table/truth_table.py
+++ truth_table/truth_table.py
@@ -28,3 +28,42 @@ class TruthTable:
             table.append(row)
 
         return table
+
+# class TruthTable:
+#     def __init__(self) -> None:
+#         self._tokens = None
+#         self._expression = None
+#         self._atoms = []
+#         self._combinations = []
+#         self._assignments = []
+#         self.truth_table = []
+
+#     def generate_truth_table(self, input_statement:str) -> List[dict[str,bool]]:
+
+#         self.reset()
+        
+#         self._tokens = lexer(input_statement)
+#         self._expression = ExpressionParser(self._tokens).parse_expression()
+#         self._atoms = extract_atoms(self._expression)
+#         self._combinations = list(product([True, False], repeat=len(self._atoms)))
+#         self._assignments = [dict(zip(self._atoms, combination)) for combination in self._combinations]
+
+#         self.truth_table = []
+
+#         for assignment in self._assignments:
+#             eval_case = self._expression.evaluate(assignment)
+#             row = {**assignment, str(self._expression):eval_case}
+#             self.truth_table.append(row)
+
+#         return self.truth_table
+    
+#     def get_truth_table(self):
+#         return self.truth_table
+    
+#     def reset(self):
+#         self._tokens = None
+#         self._expression = None
+#         self._atoms = []
+#         self._combinations = []
+#         self._assignments = []
+#         self.truth_table = []
