23d7cc6cb2adc6b364d9e588244bdcc6f57718d1
Josh <joshuahbronner@gmail.com>
2025-05-04 00:01:37 -0400

    Minor Config Changes

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import streamlit as st
 from truth_table import TruthTable
 
 if __name__ == "__main__":
+
+   st.set_page_config("Truth Table Creator")
+
+
+   st.title("Truth Table Creator")
+
    truth_table = TruthTable("AvB")
 
-   formula = st.text_input("Logical Expression:")
+   formula = st.text_input("Logical Expression:", help="Type in a logical expression here to create a truth table")
+
    
    if formula:
       st.table(TruthTable(formula).generate_truth_table())
